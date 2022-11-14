d911b82cd62232364216bcbe8b5687419b676573
ohira.hiroto <hiroto100114@gmail.com>
2022-11-14 19:03:04 +0900

    Add prettier.

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
-    return (
-        <div>
-            <h3>Hello!!</h3>
-        </div>
-    )
+  return (
+    <div>
+      <h3>Hello!!</h3>
+    </div>
+  );
 }
