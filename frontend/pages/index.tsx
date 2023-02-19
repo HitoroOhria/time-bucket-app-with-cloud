8066ab6838d941a91d8de65152e9527b65d84105
ohira.hiroto <hiroto100114@gmail.com>
2023-02-19 16:39:20 +0900

    Wrap table with card.

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,27 +1,47 @@
 import { FC } from 'react';
-import { Heading, Table, TableContainer, Tbody, Td, Text, Th, Thead, Tr } from '@chakra-ui/react';
+import {
+  Box,
+  Card,
+  CardBody,
+  CardHeader,
+  Heading,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Thead,
+  Tr,
+} from '@chakra-ui/react';
 
 const Home: FC = () => {
   return (
     <>
-      <Heading>やりたいことリスト</Heading>
-      <TableContainer>
-        <Table variant={'simple'}>
-          <Thead>
-            <Tr>
-              <Th>やりたいこと</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            <Tr>
-              <Td>海外旅行</Td>
-            </Tr>
-            <Tr>
-              <Td>東京でデート</Td>
-            </Tr>
-          </Tbody>
-        </Table>
-      </TableContainer>
+      <Card m={'20px'}>
+        <CardHeader>
+          <Heading size={'md'}>やりたいことリスト</Heading>
+        </CardHeader>
+        <CardBody>
+          <TableContainer>
+            <Table variant={'simple'}>
+              <Thead>
+                <Tr>
+                  <Th>やりたいこと</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                <Tr>
+                  <Td>海外旅行</Td>
+                </Tr>
+                <Tr>
+                  <Td>東京でデート</Td>
+                </Tr>
+              </Tbody>
+            </Table>
+          </TableContainer>
+        </CardBody>
+      </Card>
     </>
   );
 };
