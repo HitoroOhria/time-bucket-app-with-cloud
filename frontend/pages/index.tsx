5d0422b17f2eac9ac727a0515924218433061006
ohira.hiroto <hiroto100114@gmail.com>
2023-02-19 16:19:20 +0900

    Add Chakra UI.

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,7 +1,13 @@
-export default function Home() {
+import { FC } from 'react';
+import { Text } from '@chakra-ui/react';
+
+const Home: FC = () => {
   return (
     <div>
       <h3>Hello!!</h3>
+      <Text fontSize={'xl'}>Chakra UI is working!</Text>
     </div>
   );
-}
+};
+
+export default Home;
