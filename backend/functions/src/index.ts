79bb4c2a40e51d095bdc9268e074d447059d52b7
ohira.hiroto <hiroto100114@gmail.com>
2023-02-25 22:15:42 +0900

    Fix to not use require firebase-admin.

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -2,16 +2,17 @@ import { WriteResult } from 'firebase-admin/firestore';
 import { Request, Response } from 'firebase-functions';
 import { EventContext } from 'firebase-functions/lib/v1/cloud-functions';
 import { QueryDocumentSnapshot } from 'firebase-functions/lib/v1/providers/firestore';
+import { initializeApp } from 'firebase-admin/app';
+import { firestore } from 'firebase-admin';
 import functions = require('firebase-functions');
-import admin = require('firebase-admin');
 
-admin.initializeApp();
+initializeApp();
 
 exports.addMessage = functions.https.onRequest(
   async (req: Request, res: Response): Promise<void> => {
     const original = req.query.text;
 
-    const snapshot = await admin.firestore().collection('messages').add({ original: original });
+    const snapshot = await firestore().collection('messages').add({ original: original });
 
     res.json({ result: `Message with ID: ${snapshot.id} added.` });
   }
