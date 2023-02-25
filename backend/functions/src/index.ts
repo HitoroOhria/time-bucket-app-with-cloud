9636b4791ae75887f8daa2a2164fbe244b07b0b8
ohira.hiroto <hiroto100114@gmail.com>
2023-02-25 21:51:47 +0900

    Make index.ts to import only.

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,30 +1,30 @@
-import { WriteResult } from 'firebase-admin/firestore';
+import { initializeApp } from 'firebase-admin/lib/firebase-namespace-api';
+import { firestore } from 'firebase-admin/lib/firestore/firestore-namespace';
 import { Request, Response } from 'firebase-functions';
+import { log } from 'firebase-functions/lib/logger';
 import { EventContext } from 'firebase-functions/lib/v1/cloud-functions';
-import { QueryDocumentSnapshot } from 'firebase-functions/lib/v1/providers/firestore';
-import functions = require('firebase-functions');
-import admin = require('firebase-admin');
+import { onRequest } from 'firebase-functions/lib/v1/providers/https';
+import { document, QueryDocumentSnapshot } from 'firebase-functions/lib/v1/providers/firestore';
+import { WriteResult } from 'firebase-admin/firestore';
 
-admin.initializeApp();
+initializeApp();
 
-exports.addMessage = functions.https.onRequest(
-  async (req: Request, res: Response): Promise<void> => {
-    const original = req.query.text;
+export const addMessage = onRequest(async (req: Request, res: Response): Promise<void> => {
+  const original = req.query.text;
 
-    const snapshot = await admin.firestore().collection('messages').add({ original: original });
+  const snapshot = await firestore().collection('messages').add({ original: original });
 
-    res.json({ result: `Message with ID: ${snapshot.id} added.` });
-  }
-);
+  res.json({ result: `Message with ID: ${snapshot.id} added.` });
+});
 
-exports.makeUppercase = functions.firestore
-  .document('/messages/{documentId}')
-  .onCreate((snap: QueryDocumentSnapshot, context: EventContext): Promise<WriteResult> => {
+export const makeUppercase = document('/messages/{documentId}').onCreate(
+  (snap: QueryDocumentSnapshot, context: EventContext): Promise<WriteResult> => {
     const original = snap.data().original;
 
-    functions.logger.log('Uppercasing', context.params.documentId, original);
+    log('Uppercasing', context.params.documentId, original);
 
     const uppercase = original.toUpperCase();
 
     return snap.ref.set({ uppercase }, { merge: true });
-  });
+  }
+);
