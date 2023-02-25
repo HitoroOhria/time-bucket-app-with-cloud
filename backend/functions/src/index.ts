965409e524b5522572575799ed082f58918d1b47
ohira.hiroto <hiroto100114@gmail.com>
2023-02-25 21:41:06 +0900

    Format files.

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -1,35 +1,30 @@
-import { Request, Response } from "firebase-functions";
-import { EventContext } from "firebase-functions/lib/v1/cloud-functions";
-import { QueryDocumentSnapshot } from "firebase-functions/lib/v1/providers/firestore";
+import { WriteResult } from 'firebase-admin/firestore';
+import { Request, Response } from 'firebase-functions';
+import { EventContext } from 'firebase-functions/lib/v1/cloud-functions';
+import { QueryDocumentSnapshot } from 'firebase-functions/lib/v1/providers/firestore';
+import functions = require('firebase-functions');
+import admin = require('firebase-admin');
 
-const functions = require("firebase-functions");
-
-const admin = require("firebase-admin");
 admin.initializeApp();
 
 exports.addMessage = functions.https.onRequest(
   async (req: Request, res: Response): Promise<void> => {
     const original = req.query.text;
 
-    const snapshot = await admin
-      .firestore()
-      .collection("messages")
-      .add({ original: original });
+    const snapshot = await admin.firestore().collection('messages').add({ original: original });
 
     res.json({ result: `Message with ID: ${snapshot.id} added.` });
   }
 );
 
 exports.makeUppercase = functions.firestore
-  .document("/messages/{documentId}")
-  .onCreate(
-    (snap: QueryDocumentSnapshot, context: EventContext): Promise<any> => {
-      const original = snap.data().original;
+  .document('/messages/{documentId}')
+  .onCreate((snap: QueryDocumentSnapshot, context: EventContext): Promise<WriteResult> => {
+    const original = snap.data().original;
 
-      functions.logger.log("Uppercasing", context.params.documentId, original);
+    functions.logger.log('Uppercasing', context.params.documentId, original);
 
-      const uppercase = original.toUpperCase();
+    const uppercase = original.toUpperCase();
 
-      return snap.ref.set({ uppercase }, { merge: true });
-    }
-  );
+    return snap.ref.set({ uppercase }, { merge: true });
+  });
