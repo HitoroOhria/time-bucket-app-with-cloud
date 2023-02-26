7eb1a1f51612d54666d7a37848fcfb733460bb64
ohira.hiroto <hiroto100114@gmail.com>
2023-02-26 20:55:38 +0900

    Fix undefined of firestore model to it invalid.

--- backend/src/infrastructure/model/ExampleModel.ts
+++ backend/src/infrastructure/model/ExampleModel.ts
@@ -2,12 +2,12 @@ import { Example } from '../../domain/entity/Example';
 
 export type ExampleModel = {
   text: string;
-  uppercase?: string;
+  uppercase: string;
 };
 
 export const exampleEntityToModel = (entity: Example): ExampleModel => {
   return {
     text: entity.text,
-    uppercase: entity.uppercase,
+    uppercase: entity.uppercase ?? '',
   };
 };
